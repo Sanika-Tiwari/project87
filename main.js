8c6ceed6ce442abae168b9d7817c569c33d0e939
Sanika-Tiwari <thesanikatiwari@gmail.com>
2022-08-08 12:55:43 +0800

    edits

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-// Create canvas variable
+var canvas = new fabric.Canvas('myCanvas');
  block_y=1;
  block_x=1;
 
@@ -10,7 +9,18 @@ var block_image_object= "";
 
 function new_image(get_image)
 {
-	// to upload images
+	fabric.Image.fromURL(get_image, function(Img)
+	{
+		block_image_object = Img;
+
+		block_image_object.scaleToWidth(block_image_width);
+		block_image_object.scaleToHeight(block_image_height);
+		block_image_object.set({
+			top:block_y,
+			left:block_x
+		});
+		canvas.add(block_image_object);
+	});
 }
 
 window.addEventListener("keydown", my_keydown);
@@ -20,30 +30,30 @@ function my_keydown(e)
 keyPressed = e.keyCode;
 console.log(keyPressed);
 
-	if(keyPressed == '?') // add appropriate keycode
+	if(keyPressed == '82')
 	{
-		// upload red ranger
+		new_image('rr1.png')
 	}
 	if(keyPressed == '71')
 	{
 		block_x = 200;
-		// upload green ranger
+		new_image('gr.png')
 	}
 	
 	if(keyPressed == '89')
 	{
 		block_x =350;
-		// upload yellow ranger
+		new_image('yr.png')
 	}
 	if(keyPressed == '80')
 	{
 		block_x = 600;
-		// upload pink ranger
+		new_image('pr.png')
 	}
 	if(keyPressed == '66')
 	{
 		block_x = 700;
-	// upload blue ranger
+		new_image('br.png')
 	}
 	
 }
